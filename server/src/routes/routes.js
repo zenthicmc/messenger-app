7425481b26654a8dbd356049d65ac4d22f6c0208
Kuro Cybria <rizqikurniawan1863@gmail.com>
2022-07-10 22:20:08 +0700

    update commit
    add server for backend

---
+++ server/src/routes/routes.js
@@ -0,0 +1,47 @@
+const express = require('express')
+require('express-router-group')
+const router = express.Router();
+const userController = require('../controllers/userController');
+const userValidator = require('../middlewares/validators/userValidator');
+const multer = require('multer');
+const randomstring = require('randomstring');
+
+router.get('/', async (req, res) => {
+    res.send("root");
+})
+
+// USER API GROUP
+router.group('/api/user', router => {
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, './public/images/users')
+        },
+        filename: function (req, file, cb) {
+            cb(null, randomstring.generate(15) + '.' + file.mimetype.split('/')[1])
+        }
+    });
+
+    const upload = multer({ storage: storage });
+
+    // router.get('/user', userController.add);
+
+    router.get('/', userController.index);
+
+    // Validation
+    router.get('/username/:username', userValidator.validateUsername);
+
+    router.get('/email/:email', userValidator.validateEmail);
+
+    // Controller
+    router.get('/:username', userController.details);
+    
+    router.post('/', upload.single('image'), userController.store);
+
+    router.put('/:username', upload.single('image'), userController.update);
+
+    router.delete('/:username', userController.destroy);
+
+    router.get('/:username/image', userController.viewImage);
+});
+
+module.exports = { router }
