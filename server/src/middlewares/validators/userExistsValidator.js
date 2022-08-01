cf25bfd841559a1ab99b74e826c9d1673d87eac7
Kuro Cybria <rizqikurniawan1863@gmail.com>
2022-08-01 16:50:29 +0700

    Update User Validator
    Validator bug fixed

--- server/src/middlewares/validators/userExistsValidator.js
+++ server/src/middlewares/validators/userExistsValidator.js
@@ -1,22 +1,17 @@
 require('../../../config/database');
 const User = require('../../models/User');
-const { body, check, validationResult } = require('express-validator');
 
 exports.validateUsername = async (req, res, next) => {
-    body('username').custom(async (username) => {
-        const data = await User.findOne({ username: username });
+    const data = await User.findOne({ username: req.params.username });
 
-        if (data) {
-            return res.json({ status: 'fail', message: 'Username Sudah Terdaftar' });
-        }
+    if (data) {
+       return res.json ({ status: 'fail', message: 'Username Sudah Terdaftar' });
+    }
 
-        return res.json({ status: 'success', message: 'Username Dapat Digunakan' });
-    })
-
-
+    return res.json({ status: 'success', message: 'Username Dapat Digunakan' });
 }
 
-exports.validateEmail = check('email', 'Email Tidak Valid').isEmail, async (req, res, next) => {
+exports.validateEmail = async (req, res, next) => {
     const data = await User.findOne({ email: req.params.email });
 
     if (data) {
@@ -24,15 +19,4 @@ exports.validateEmail = check('email', 'Email Tidak Valid').isEmail, async (req,
     }
 
     return res.json({ status: 'success', message: 'Email Dapat Digunakan' });
-
 }
-
-// exports.validateEmail = async (req, res, next) => {
-//     const data = await User.findOne({ email: req.params.email });
-
-//     if (data) {
-//         return res.json({ status: 'fail', message: 'Email Sudah Terdaftar' });
-//     }
-
-//     return res.json({ status: 'success', message: 'Email Dapat Digunakan' });
-// }
