43d8d04bd77700cfa1ef289b04d3d4b18f6993a4
Kuro Cybria <rizqikurniawan1863@gmail.com>
2022-09-03 12:35:58 +0700

    Update Minor

--- frontend/src/Components/StartChat.jsx
+++ frontend/src/Components/StartChat.jsx
@@ -5,8 +5,8 @@ const StartChat = () => {
       <div className="m-auto">
          <img className="start-chat-img" src={startchat} alt="startchat" />
 			<div className='start-chat-text w-100 p-3'>
-				<h5 className="text-center fw-bold">Select A User To Start Chatting With</h5>
-				<p className='text-center t-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis animi temporibus quis, nemo sunt quam?</p>
+				<h5 className="text-center fw-bold">Select A User To Start Conversation</h5>
+				
 			</div>
       </div>
    );
